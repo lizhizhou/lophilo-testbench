38862aa88eac951ebb1e523bd2431dd0023c7577
Ricky Ng-Adam <rngadam@lophilo.com>
2012-11-07 05:20:59 +0800

    break down into functions

--- experiments/testmmcblk1.js
+++ experiments/testmmcblk1.js
@@ -1,16 +1,23 @@
 'use strict';
 
 var fs = require('fs');
+var sprintf = require('sprintf').sprintf;
 var assert = require('should');
 
-var disksize = parseInt(fs.readFileSync('/sys/class/block/mmcblk1/size').toString())*512;
-var blocksize = 4096;
-var testString = '0123456789ABCDEF';
-var stringCount = 4096;
-var buffer = new Buffer(testString.length*stringCount);
+function getBlockDeviceSize(name) {
+  var sysFileSize = sprintf('/sys/class/block/%s/size', name)
+  return parseInt(fs.readFileSync(sysFileSize).toString())*512;
+}
 
-for(var i=0; i<stringCount; i++) {
-  buffer.write(testString, i*testString.length, testString.length, 'ascii');
+function getTestBuffer(size) {
+  var testString = '0123456789ABCDEF';
+  assert((size%testString.length) === 0, '' + size + ' is not a multiple of ' + testString.length);
+  var stringCount = size/testString.length;
+  var buffer = new Buffer(testString.length*stringCount);  
+  for(var i=0; i<stringCount; i++) {
+    buffer.write(testString, i*testString.length, testString.length, 'ascii');
+  }  
+  return buffer;
 }
 
 function StatsCollector(total) {
@@ -25,9 +32,9 @@ function StatsCollector(total) {
     var delta = process.hrtime(self.lastTime);
     if(delta[0] > 5) {
       self.count++;
-      var bps = (i - self.lastCount)/(delta[0]);
-      self.totalBps += bps;
-      var mbps = Math.round(bps / (1024*1024));
+      self.latestBps = (i - self.lastCount)/(delta[0]);
+      self.totalBps += self.latestBps;
+      var mbps = Math.round(self.latestBps / (1024*1024));
       var completion = Math.round(((i/self.total)*100));
       console.log('bytes: ' + i + ', completed ' + completion + '%, mbps ' + mbps);
 
@@ -36,17 +43,18 @@ function StatsCollector(total) {
     }
   }
 }
-function writeTestData(filename) {
+
+function writeTestData(filename, disksize, testBuffer) {
   var stats = new StatsCollector(disksize);
   var written = 0;
   console.log('disk size ' + disksize);
-  console.log('buffer length ' + buffer.length);
+  console.log('buffer length ' + testBuffer.length);
 
   var fd = fs.openSync(filename, 'w')
-  for(var i=0; i<disksize; i+=buffer.length) {
+  for(var i=0; i<disksize; i+=testBuffer.length) {
     written = 0;
-    while(written !== buffer.length) {
-      written += fs.writeSync(fd, buffer, written, buffer.length - written, i + written);  
+    while(written !== testBuffer.length) {
+      written += fs.writeSync(fd, testBuffer, written, testBuffer.length - written, i + written);  
     }
 
     stats.update(i);
@@ -56,28 +64,31 @@ function writeTestData(filename) {
   console.log('write completed in ' + delta);  
 }
 
-function checkTestData(filename) {
+function checkTestData(filename, disksize, testBuffer) {
   var stats = new StatsCollector(disksize);
 
-  var readBuffer = new Buffer(buffer.length);
+  var readBuffer = new Buffer(testBuffer.length);
   var fd = fs.openSync(filename, 'r')
   var read = 0;
-  for(var i=0; i<disksize; i+=buffer.length) {
+  for(var i=0; i<disksize; i+=testBuffer.length) {
     read = 0
-    while(read !== buffer.length) {
-      read += fs.readSync(fd, readBuffer, read, buffer.length - read, i + read); 
+    while(read !== testBuffer.length) {
+      read += fs.readSync(fd, readBuffer, read, testBuffer.length - read, i + read); 
     }
-    assert(readBuffer.toString() === buffer.toString(), 'failed check at position ' + (i+read));
+    assert(readBuffer.toString() === testBuffer.toString(), 'failed check at position ' + (i+read));
     stats.update(i);    
   }  
 }
 
 
 if (require.main === module) {
+  var filename = '/dev/mmcblk1';
+  var disksize = getBlockDeviceSize('mmcblk1');
+  var testBuffer = getTestBuffer(64*1024);
   if(process.argv.indexOf('write') !== -1) {
-    writeTestData();
+    writeTestData(filename, disksize, testBuffer);
   }
   if(process.argv.indexOf('check') !== -1) {
-    checkTestData('/dev/mmcblk1');
+    checkTestData(filename, disksize, testBuffer);
   }
 } 
