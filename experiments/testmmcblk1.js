bf89d1b1a6b4a63c25e0552fa2afa017525e7d60
Ricky Ng-Adam <rngadam@lophilo.com>
2012-11-07 04:58:48 +0800

    with stats collector

--- experiments/testmmcblk1.js
+++ experiments/testmmcblk1.js
@@ -3,18 +3,8 @@
 var fs = require('fs');
 var assert = require('should');
 
-var writeStream = fs.createWriteStream('/dev/mmcblk1', {
-  encoding: 'ascii'
-});
-
-writeStream.on('error', function(exception) {
-  console.error(exception.stack);
-  throw exception;
-});
-
 var disksize = parseInt(fs.readFileSync('/sys/class/block/mmcblk1/size').toString())*512;
 var blocksize = 4096;
-var repeatedString = '';
 var testString = '0123456789ABCDEF';
 var stringCount = 4096;
 var buffer = new Buffer(testString.length*stringCount);
@@ -23,44 +13,71 @@ for(var i=0; i<stringCount; i++) {
   buffer.write(testString, i*testString.length, testString.length, 'ascii');
 }
 
-var startTime = process.hrtime();
-var lastTime = startTime;
-var lastCount = 0;
-var written = 0;
-console.log('disk size ' + disksize);
-console.log('buffer length ' + buffer.length);
+function StatsCollector(total) {
+  var self = this;
+  self.startTime = process.hrtime();
+  self.lastTime = self.startTime;
+  self.lastCount = 0;  
+  self.total = total;
+  self.totalBps = 0;
+  self.count = 0;
+  self.update = function(i) {
+    var delta = process.hrtime(self.lastTime);
+    if(delta[0] > 5) {
+      self.count++;
+      var bps = (i - self.lastCount)/(delta[0]);
+      self.totalBps += bps;
+      var mbps = Math.round(bps / (1024*1024));
+      var completion = Math.round(((i/self.total)*100));
+      console.log('bytes: ' + i + ', completed ' + completion + '%, mbps ' + mbps);
 
-var fd = fs.openSync('/dev/mmcblk1', 'w')
-for(var i=0; i<disksize; i+=buffer.length) {
-  written = 0;
-  while(written !== buffer.length) {
-    written += fs.writeSync(fd, buffer, written, buffer.length - written, i + written);  
-  }
-
-  var delta = process.hrtime(lastTime);
-  if(delta[0] > 5) {
-    var bps = (i - lastCount)/(delta[0]);
-    var mbps = Math.round(bps / (1024*1024));
-    var completion = Math.round(((i/disksize)*100));
-    console.log('bytes written: ' + i + ' total %' + completion + ' mbps ' + mbps);
-
-    lastCount = i;
-    lastTime = process.hrtime();
+      self.lastCount = i;
+      self.lastTime = process.hrtime();
+    }
   }
 }
+function writeTestData(filename) {
+  var stats = new StatsCollector(disksize);
+  var written = 0;
+  console.log('disk size ' + disksize);
+  console.log('buffer length ' + buffer.length);
 
-fs.closeSync(fd);
+  var fd = fs.openSync(filename, 'w')
+  for(var i=0; i<disksize; i+=buffer.length) {
+    written = 0;
+    while(written !== buffer.length) {
+      written += fs.writeSync(fd, buffer, written, buffer.length - written, i + written);  
+    }
 
-var delta = process.hrtime(startTime);
-console.log('write completed in ' + delta);
-
-var readBuffer = new Buffer(buffer.length);
-fd = fs.openSync('/dev/mmcblk1', 'r')
-var read = 0;
-for(var i=0; i<disksize; i+=buffer.length) {
-  read = 0
-  while(read !== buffer.length) {
-    read += fs.readSync(fd, readBuffer, read, buffer.length - read, i + read); 
+    stats.update(i);
   }
-  assert(readBuffer.toString() === buffer.toString());
+  fs.closeSync(fd);
+  var delta = process.hrtime(startTime);
+  console.log('write completed in ' + delta);  
 }
+
+function checkTestData(filename) {
+  var stats = new StatsCollector(disksize);
+
+  var readBuffer = new Buffer(buffer.length);
+  var fd = fs.openSync(filename, 'r')
+  var read = 0;
+  for(var i=0; i<disksize; i+=buffer.length) {
+    read = 0
+    while(read !== buffer.length) {
+      read += fs.readSync(fd, readBuffer, read, buffer.length - read, i + read); 
+    }
+    assert(readBuffer.toString() === buffer.toString(), 'failed check at position ' + (i+read));
+    stats.update(i);    
+  }  
+}
+
+
+if (require.main === module) {
+  if(process.argv.indexOf('write') !== -1) {
+    writeTestData();
+  }
+  if(process.argv.indexOf('check') !== -1) {
+    checkTestData('/dev/mmcblk1');
+  }
+} 
