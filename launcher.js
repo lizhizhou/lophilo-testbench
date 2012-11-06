92c2a400e125660ca825d855a93e72e605037689
Ricky Ng-Adam <rngadam@lophilo.com>
2012-11-06 12:16:41 +0800

    Replace Polo by the native avahi bindings.

--- launcher.js
+++ launcher.js
@@ -5,20 +5,30 @@ var fs = require('fs');
 var http = require('http');
 var os = require('os');
 var path = require('path');
+var dns = require('dns');
+var mdns = require('mdns');
 
 var spawn = require('child_process').spawn;
 var sprintf = require('sprintf').sprintf;
 
-var lmcLophiloPath = '/dnode?require=lophilo'
+var LMC_LOPHILO_PATH = '/dnode?require=lophilo'
+
+var processed = {};
 
 function getHWAddress(ip) {
-  console.log('matching ' + ip);
+  //console.log('matching ' + ip);
   var interfaces = os.networkInterfaces();
   var hwaddress;
   var found = Object.keys(interfaces).some(function(interfaceName) {
+    // console.log(interfaceName);
     return interfaces[interfaceName].some(function(potentialMatch) {
+      // console.log(potentialMatch);
       if(potentialMatch.address === ip) {
-        hwaddress = fs.readFileSync(sprintf('/sys/class/net/%s/address', interfaceName)).toString();
+        var interfaceFile = sprintf('/sys/class/net/%s/address', interfaceName);
+        hwaddress = fs.readFileSync(interfaceFile)
+            .toString()
+            .match(/[\w:]+/g)[0];
+        // console.log('match ' + hwaddress);
         return true;
       }
     });
@@ -29,40 +39,48 @@ function getHWAddress(ip) {
     var arps = arpFileContent.toString().split('\n');
     found = arps.some(function(arp) {
       if(arp.indexOf(ip) === 0) {
-        hwaddress = arp.match(/\w+/g)[3];
+        // console.log('match! ' + arp);
+        hwaddress = arp.match(/[\w:\.]+/g)[3];
         return true;
       }
     });
   }
   assert(found, 'could not find a match for ' + ip);
+  assert('74:e5:0b:0d:19:be'.length == hwaddress.length, 'incorrect ' + hwaddress);
   return hwaddress;
 }
 
-function launchMocha(err, hostname, port) {
+function launchMocha(err, ip, port) {
   var mochaPath = require.resolve('mocha');
   var mochaBinPath = path.resolve(path.join(mochaPath, '..', 'bin', 'mocha'));
-  var mac = getHWAddress(hostname);
+  var mac = processed[ip];
+
   console.log('mac: ' + mac);
   console.log('using ' + mochaBinPath);
+
+  var log = fs.openSync(sprintf('./%s.log', mac), 'w+');
   var child = spawn(
     process.execPath, // path to node
-    [mochaBinPath, 'test/'],
+    [mochaBinPath, '--reporter', 'json', 'test/'],
     {
-      stdio: 'inherit',
+      stdio: ['ignore', log, log],
+      //stdio: 'inherit',
       cwd: __dirname,
       env: {
-        LOPHILO_IP: hostname,
+        LOPHILO_IP: ip,
         LOPHILO_PORT: port
       }
     }
   );
   child.on('exit', function() {
-    process.exit();
+    console.log('child exited for ' + mac);
+    //process.exit();
   })
 }
 
-function getDnodeHostnamePort(lmcHostname, lmcPort, dnodePath, cb) {
-  http.get({host: lmcHostname, port: lmcPort, path: dnodePath}, function(res) {
+function getDnodeHostnamePort(lmcIP, lmcPort, dnodePath, cb) {
+  console.log('fetching dnode from ' + lmcIP);
+  var req = http.get({host: lmcIP, port: lmcPort, path: dnodePath}, function(res) {
     var data = '';
     res.on('data', function (chunk) {
       data += chunk;
@@ -71,22 +89,40 @@ function getDnodeHostnamePort(lmcHostname, lmcPort, dnodePath, cb) {
     res.on('end', function() {
       if(res.statusCode >= 200 && res.statusCode < 400) {
         var port = parseInt(data);
-        cb(null, lmcHostname, port)
+        cb(null, lmcIP, port);
       }
     });
+  });
+  req.on('error', function(err) {
+    console.log('error connecting to %s:%d', lmcIP, lmcPort);
   });
 }
 
 if (require.main === module) {
-  var polo = require('polo');
-  var apps = polo();
-  // up fires everytime some service joins
-  apps.once('up', function(name, service) {
-    // should print out the joining service
-    console.log(apps.get('http://{lmc}'));
-    var lmc = apps.get('lmc');
-    if(lmc) {
-      getDnodeHostnamePort(lmc.host, lmc.port, lmcLophiloPath, launchMocha);
+  var browser = mdns.createBrowser(mdns.tcp('http'));
+  browser.on('serviceUp', function(service) {
+    if(service.name.indexOf('lmc') === 0) {
+      var ip = service.addresses[0];
+      var mac = getHWAddress(ip);
+      if(processed[mac]) {
+        console.log('already processed ' + mac);
+        return;
+      }
+      processed[mac] = ip;
+      processed[ip] = mac;
+
+      console.log('service ', service);
+      getDnodeHostnamePort(
+        ip,
+        service.port,
+        LMC_LOPHILO_PATH,
+        launchMocha);
     }
   });
+  browser.on('serviceDown', function(service) {
+    console.log("service down: ", service);
+  });
+  browser.start();
+  var server = http.createServer();
+  server.listen(0);
 }
