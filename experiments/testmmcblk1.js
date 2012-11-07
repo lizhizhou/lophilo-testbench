f6b04f99b8f528ca5720c7bff9317aead2ed4521
Ricky Ng-Adam <rngadam@lophilo.com>
2012-11-07 08:17:31 +0800

    data collection successful, still showing false errors

--- experiments/testmmcblk1.js
+++ experiments/testmmcblk1.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var fs = require('fs');
-var sprintf = require('sprintf').sprintf;
 var assert = require('should');
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var sprintf = require('sprintf').sprintf;
 
-function getBlockDeviceSize(name) {
+function getBlockDeviceSize(filename) {
+  var name = path.basename(filename);
   var sysFileSize = sprintf('/sys/class/block/%s/size', name)
   return parseInt(fs.readFileSync(sysFileSize).toString())*512;
 }
@@ -20,75 +23,184 @@ function getTestBuffer(size) {
   return buffer;
 }
 
-function StatsCollector(total) {
+function StatsCollector(total, countInit, updateFrequency) {
   var self = this;
   self.startTime = process.hrtime();
-  self.lastTime = self.startTime;
-  self.lastCount = 0;  
+  self.lastTimeUpdate = self.startTime;
+  self.lastTimeDisplay = self.startTime;
+  self.lastCount = countInit;  
+  self.updateFrequency = updateFrequency ? updateFrequency: 2;
   self.total = total;
   self.totalBps = 0;
   self.count = 0;
   self.update = function(i) {
-    var delta = process.hrtime(self.lastTime);
-    if(delta[0] > 5) {
-      self.count++;
-      self.latestBps = (i - self.lastCount)/(delta[0]);
-      self.totalBps += self.latestBps;
-      var mbps = Math.round(self.latestBps / (1024*1024));
-      var completion = Math.round(((i/self.total)*100));
-      console.log('bytes: ' + i + ', completed ' + completion + '%, mbps ' + mbps);
+    var deltaUpdate = process.hrtime(self.lastTimeUpdate);
+    self.count++;
+    self.latestBps = (i - self.lastCount)/(deltaUpdate[0]+(deltaUpdate[1]/1e9));
+    self.totalBps += self.latestBps;    
+    self.lastCount = i;
+    self.lastTimeUpdate = process.hrtime();
 
-      self.lastCount = i;
-      self.lastTime = process.hrtime();
+    var deltaDisplay = process.hrtime(self.lastTimeDisplay);
+    if((deltaDisplay[0]+1) > self.updateFrequency) {
+      self.lastTimeDisplay = process.hrtime();
+      var mbps = (self.latestBps / (1024*1024)).toPrecision(2);
+      var completion =(((i/self.total)*100)).toPrecision(2);
+      console.log('bytes: ' + i + ', completed ' + completion + '%, mbps ' + mbps);
     }
+  }
+  self.getAverageBps = function() {
+    return (self.totalBps / self.count);
+  }
+  self.getElapsedSeconds = function() {
+    var delta = process.hrtime(self.startTime);
+    // always round up
+    return delta[0]+1;
+  }
+  self.bench = function() {
+    self.bench = process.hrtime();
+  }
+  self.mark = function(str) {
+    console.log(str + ': ' + process.hrtime(self.bench));
   }
 }
 
-function writeTestData(filename, disksize, testBuffer) {
-  var stats = new StatsCollector(disksize);
+function writeTestData(filename, diskSize, testBuffer, startOffset, maxElapsedSeconds) {
+  startOffset = startOffset ? startOffset : 0;
+  var stats = new StatsCollector(diskSize, startOffset, Math.ceil(maxElapsedSeconds/2));
   var written = 0;
-  console.log('disk size ' + disksize);
-  console.log('buffer length ' + testBuffer.length);
 
-  var fd = fs.openSync(filename, 'w')
-  for(var i=0; i<disksize; i+=testBuffer.length) {
+  console.log('write startOffset ' + startOffset);
+
+  var fd = fs.openSync(filename, 'rs+')
+  var loop = true;
+  for(var i=startOffset; (i+testBuffer.length)<diskSize && loop; i+=testBuffer.length) {
     written = 0;
     while(written !== testBuffer.length) {
       written += fs.writeSync(fd, testBuffer, written, testBuffer.length - written, i + written);  
     }
 
-    stats.update(i);
+    stats.update(i+testBuffer.length);
+    if(maxElapsedSeconds && stats.getElapsedSeconds() >= maxElapsedSeconds) {
+      loop = false;
+    }
   }
+  console.log('write completed in ' + stats.getElapsedSeconds());  
+  stats.bench()  
   fs.closeSync(fd);
-  var delta = process.hrtime(startTime);
-  console.log('write completed in ' + delta);  
+  stats.mark('fs.closeSync');
+
+  return {
+    stats: stats,
+    start: startOffset,
+    end: i
+  };
 }
 
-function checkTestData(filename, disksize, testBuffer) {
-  var stats = new StatsCollector(disksize);
+function checkTestData(filename, diskSize, testBuffer, startOffset, endOffset) {
+  startOffset = startOffset ? startOffset : 0;
+
+  var stats = new StatsCollector(diskSize, startOffset, 1);
+
+  console.log('read startOffset ' + startOffset);
 
   var readBuffer = new Buffer(testBuffer.length);
-  var fd = fs.openSync(filename, 'r')
+  var fd = fs.openSync(filename, 'rs')
   var read = 0;
-  for(var i=0; i<disksize; i+=testBuffer.length) {
+  var errors = [];
+  for(var i=startOffset; i<endOffset; i+=testBuffer.length) {
     read = 0
     while(read !== testBuffer.length) {
       read += fs.readSync(fd, readBuffer, read, testBuffer.length - read, i + read); 
     }
-    assert(readBuffer.toString() === testBuffer.toString(), 'failed check at position ' + (i+read));
-    stats.update(i);    
+    if(readBuffer.toString() === testBuffer.toString()) {
+      errors.push('failed check at position ' + i);  
+    }
+    
+    stats.update(i+testBuffer.length);    
   }  
+  return {
+    errors: errors,
+    stats: stats,
+  }
 }
 
+function getBlockDeviceInformation(filename) {
+  var name = path.basename(filename);
+  var deviceDirectory = sprintf('/sys/class/block/%s/device', name);
+  var infos = {}
+  fs.readdirSync(deviceDirectory).forEach(function(file) {
+    filename = path.join(deviceDirectory, file);
+    if(!fs.statSync(filename).isFile())
+      return;
+    var elements = fs.readFileSync(filename).toString().split('\n')
+    elements.pop(); // remove last \n
+    infos[file] = elements.join(';');
+  });
+  return infos;
+}
+
+function executeTest(filename) {
+  var diskSize = getBlockDeviceSize(filename);
+  var infos = getBlockDeviceInformation(filename);
+  infos.diskSize = diskSize;
+
+  var estimatedMaxMbps = 6*1024*1024;
+  var maxTime = 30;
+  var blockTime = 5;
+  var testBuffer = getTestBuffer(64*1024);
+  var writeResults = [];
+  
+  console.log('infos ' + JSON.stringify(infos, null, 4));
+  console.log('disk size ' + diskSize);
+  console.log('buffer length ' + testBuffer.length);
+
+  if(process.argv.indexOf('write') !== -1) {
+    var sliceSize = Math.floor(diskSize/((maxTime/blockTime)));
+    for(var i=0; (i+sliceSize)<diskSize; i += sliceSize) {
+      writeResults.push(writeTestData(filename, diskSize, testBuffer, i, blockTime));
+    }    
+  }
+  var errors;
+  var readResults = [];
+  if(process.argv.indexOf('check') !== -1) {
+    writeResults.forEach(function(r) {
+      readResults.push(checkTestData(filename, diskSize, testBuffer, r.start, r.end));
+    });    
+  }
+  
+  var writeDataPoints = [];
+  writeResults.forEach(function(result) {
+    writeDataPoints.push(result.stats.getAverageBps());
+  });
+
+  var readDataPoints = [];
+  var readErrors = [];
+  readResults.forEach(function(result) {
+    readDataPoints.push(result.stats.getAverageBps());
+    readErrors = readErrors.concat(result.errors);
+  });
+
+  infos.test = {};
+  infos.test.readBps = readDataPoints;
+  infos.test.writeBps = writeDataPoints;
+  infos.test.maxTime = maxTime;
+  infos.test.blockTime = blockTime;  
+  infos.test.bufferSize = testBuffer.length;  
+  infos.test.versions = process.versions;
+  infos.test.arch = process.arch;
+  infos.test.kernel = os.release();
+  infos.test.readErrors = readErrors;
+  infos.test.sliceSize = sliceSize;
+
+  return infos;
+}
+
+exports.executeTest = executeTest;
 
 if (require.main === module) {
   var filename = '/dev/mmcblk1';
-  var disksize = getBlockDeviceSize('mmcblk1');
-  var testBuffer = getTestBuffer(64*1024);
-  if(process.argv.indexOf('write') !== -1) {
-    writeTestData(filename, disksize, testBuffer);
-  }
-  if(process.argv.indexOf('check') !== -1) {
-    checkTestData(filename, disksize, testBuffer);
-  }
+  var infos = executeTest(filename);
+  fs.writeFileSync(infos.name + '.log', JSON.stringify(infos, null, 4));
+  process.exit(infos.test.readErrors);
 } 
