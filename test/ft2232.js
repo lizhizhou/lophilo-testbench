dbef1e41ba4673355c6fa3ec5befbec2d016b273
Ricky Ng-Adam <rngadam@lophilo.com>
2012-11-08 07:37:01 +0800

    usb loopback test (using serial communication to console)

---
+++ test/ft2232.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var crypto = require('crypto');
+var assert = require('should');
+var dnodeloader = require('dnode-dynamicloader');
+
+function dnodeError(err) {
+  if(!(err instanceof Error)) {
+    return new Error(err.message);
+  } else {
+    return err;
+  }
+}
+
+describe('usb host/client ft2232', function() {
+  var lcons;
+
+  before(function(done) {
+    dnodeloader.require('lophilo-console', process.env.LMC_IP, parseInt(process.env.LMC_PORT), 
+      function(err, remoteObject) {
+        if(err) return done(err);
+        lcons = remoteObject;
+        lcons.cleanup();
+        lcons.setup(function(err) {
+          if(err) return done(dnodeError(err));
+          done();
+        })
+    });
+  });
+
+  beforeEach(function(done) {
+    // get rid of any garbage that could be left on the command-line
+    lcons.writeReadUntilDelimiter('\n', function(err, data) {
+      if(err) return done(dnodeError(err));
+      done();      
+    });      
+  });
+
+  it('/etc/debian_version is correct', function(done) {
+    lcons.writeReadUntilDelimiter("cat /etc/debian_version\n", function(err, data) {
+      if(err) return done(dnodeError(err));
+      if(data.indexOf('wheezy/sid') === -1) {
+        return done(new Error('got ' + data));
+      }
+      done();
+    });
+  });
+
+  /*
+  it('catting binary file work', function(done) {
+    lcons.writeReadUntilDelimiter("md5sum /etc/debian_version\n", function(err, data) {
+      console.log(data);
+      if(err) return done(dnodeError(err));
+      var expectedMD5 = data.match(/\w+/)[0];
+      lcons.writeReadUntilDelimiter("cat /etc/debian_version\n", function(err, data) {
+        var md5summer = crypto.createHash('md5');
+        console.log(data);
+        md5summer.update();
+        var actualMD5 = md5summer.digest('hex');
+        if(actualMD5 !== expectedMD5) {
+          done(new Error(actualMD5 + ' does not match ' + expectedMD5));
+        } else {
+          done();  
+        }
+        
+      });
+    });
+  });
+  */
+
+  after(function() {
+    //lophilo.dnode.end();
+    lcons.cleanup();
+  });
+});
+
