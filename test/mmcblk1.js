878dd613a5dfc1c99969c8b010d6a4843aee844f
Ricky Ng-Adam <rngadam@lophilo.com>
2012-11-09 13:09:06 +0800

    All green: use fork for the sync-heavy mmcblk1.

--- test/mmcblk1.js
+++ test/mmcblk1.js
@@ -8,7 +8,7 @@ describe('iotestbench', function() {
   var iotestbench;
 
   before(function(done) {
-    dnodeloader.require('iotestbench', process.env.LMC_IP, parseInt(process.env.LMC_PORT), 
+    dnodeloader.requireFork('iotestbench', process.env.LMC_IP, parseInt(process.env.LMC_PORT), 
       function(err, remoteObject) {
         if(err) return done(err);
         iotestbench = remoteObject;
@@ -19,7 +19,7 @@ describe('iotestbench', function() {
   it('no errors reading/writing sdcard', function(done) {
     var maxTime = 15;
     var blockTime = 5;
-    this.timeout((maxTime*2+blockTime)*1000);
+    this.timeout((maxTime*2+blockTime)*2*1000);
     iotestbench.executeTestAsync('/dev/mmcblk1', maxTime, blockTime, function(err, result) {
       assert(!err, 'error received ' + err);
       assert(result.test.readErrors.length === 0, 'bad ' + result);
