bc7ec8e7ed0e8a272c65ec59d176bbcb575e9fee
Ricky Ng-Adam <rngadam@lophilo.com>
2012-11-25 15:46:33 +0800

    keep results for future reference

--- test/mmcblk1.js
+++ test/mmcblk1.js
@@ -3,12 +3,12 @@
 var Q = require('q');
 var assert = require('should');
 var dnodeloader = require('dnode-dynamicloader');
-
+var utils = require('./lib/utils');
 describe('iotestbench', function() {
   var iotestbench;
 
   before(function(done) {
-    dnodeloader.requireFork('iotestbench', process.env.LMC_IP, parseInt(process.env.LMC_PORT), 
+    dnodeloader.requireFork('iotestbench', process.env.LMC_IP, parseInt(process.env.LMC_PORT),
       function(err, remoteObject) {
         if(err) return done(err);
         iotestbench = remoteObject;
@@ -23,6 +23,7 @@ describe('iotestbench', function() {
     iotestbench.executeTestAsync('/dev/mmcblk1', maxTime, blockTime, function(err, result) {
       assert(!err, 'error received ' + err);
       assert(result.test.readErrors.length === 0, 'bad ' + result);
+      utils.record('mmcblk1', JSON.stringify(result, null, 4));
       done();
     });
   });
